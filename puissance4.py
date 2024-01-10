ed77a9fa6032c57df2d4a3a8959c270218401f5a
Lu-Haisia <lumetcie@gmail.com>
2024-01-10 23:26:10 +0100

    Modif : dans Jeu() cases_possible et deplacement_possibles et dans Gui() set_pion , Ajout : end_of_game, modif dans notes concernant les statuts des cases

--- puissance4.py
+++ puissance4.py
@@ -15,8 +15,6 @@ class Jeu:
 
     def set_board_size(self, board_size):
         ''' Fonction qui permet de selectionner la taille du plateau entre 8 et 12.'''
-
-
         self.__board_size = int(input("Choisissez une taille de plateau comprise entre 8 et 12 : "))
         while (8 > self.__board_size) or (self.__board_size > 12):
             self.__board_size = int(input("Taille non prise en charge, veuillez re-saisir une taille de plateau comprise entre 8 et 12 : "))
@@ -58,7 +56,7 @@ class Jeu:
         '''Permet de placer le pion où l'on veut en début de partie'''
         pass
 
-    
+
     def set_pion_game(self,):
         '''Permet de placer le pion durant la partie en fonction des case possibles'''
         pass
@@ -72,16 +70,30 @@ class Jeu:
         num = 1 
         while num != 8:
             # Case choisie = cases de déplacement et correspond a une case possible
-            if self[i][j] == self[poscases] and self[i][j] == 2:
+            if self[i][j] == self[poscases] :
                 return True
             else :
                 None 
             num +=1
+        return False 
 
 
     def deplacement_possible(self, i,j):
         '''Fonction qui, si la fontion "cases_possibles" retourne True, vérifie si la case est libre'''
+        if self.cases_possibles() == True and self[i][j] == 3 :
+            return True
+        else :
+            return False
+
+
+    def end_of_game(self):
+        '''Permet de vérifier les conditions d'arrêt'''
+        '''Si la case choisie n'est pas une case où l'on peut mettre le pion (plus le cases dispo) alors fin de la partie = vrai'''
+        if self.deplacement_possible(self) == False:
+            return True
         pass
+        
+
 
 
 class Gui(Jeu) :
@@ -103,7 +115,7 @@ class Gui(Jeu) :
                     self.__canvas.create_rectangle(i * self.__taille_case, j * self.__taille_case, i *self.__taille_case + self.__taille_case, j * self.__taille_case + self.__taille_case)
 
                     
-            #self.__canvas.bind("<Button-1>", self.update_on_clic)
+            self.__canvas.bind("<Button-1>", self.set_pions())
             #self.__canvas.bind("<Button-2>", self.start_auto_fire)
             #self.__canvas.bind("<Button-3>", self.fire_on_clic)
             
@@ -111,7 +123,13 @@ class Gui(Jeu) :
             self.__canvas.pack()
             self.__root.mainloop()
             
-        def set_pions(self):
+        def set_pions(self, event):
+            '''A ajuster pour faire en sorte d'ajouter un pion quand on clique'''
+            coord_x = event.x//self.__size_x
+            coord_y = event.y//self.__size_y
+            if self.__forest.get_tree_in_grid(coord_x, coord_y).get_state() == 1 :
+                self.__forest.set_tree_state_in_grid(coord_x, coord_y, 2)
+                self.update_grid()
             pions = []
             plateau = self.__game.get_board()
             for i in range(len(plateau)):
