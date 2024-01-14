5e21a2fa9155e5384d2c4dac3584bd805ab0ca7f
Antoine <antoine.julien@supinfo.com>
2024-01-14 13:59:21 +0100

    Tranformation des valeurs privées et protégées

--- save_all.py
+++ save_all.py
@@ -3,13 +3,13 @@ from tkinter import *
 
 class Pion:
     def __init__(self, x, y, joueur=1):
-        self.__x = x
-        self.__y = y
-        self.__joueur = joueur
-        self.__possible = []
+        self._x = x
+        self._y = y
+        self._joueur = joueur
+        self._possible = []
 
     def get_joueur(self):
-        return self.__joueur
+        return self._joueur
 
     def cases_possibles(self, plateau):
         """
@@ -17,110 +17,108 @@ class Pion:
         """
         # Vérifie si la case est sur le plateau
 
-        if self.__x-2 >= 0 and self.__y-1 >= 0:
+        if self._x-2 >= 0 and self._y-1 >= 0:
             # Vérifie si la case est disponible
-            if plateau[self.__x-2][self.__y-1] == 0 or plateau[self.__x-2][self.__y-1] == 3 or plateau[self.__x-2][self.__y-1] == 4:
+            if plateau[self._x-2][self._y-1] == 0:
                 # Mise des coordonnées en tuple pour pas qu'elles soient modifiées
-                self.__possible.append((self.__x-2, self.__y-1))
+                self._possible.append((self._x-2, self._y-1))
 
-        if self.__x-2 >= 0 and self.__y+1 < len(plateau):
+        if self._x-2 >= 0 and self._y+1 < len(plateau):
             # Vérifie si la case est disponible
-            if plateau[self.__x-2][self.__y+1] == 0 or plateau[self.__x-2][self.__y-1] == 3 or plateau[self.__x-2][self.__y-1] == 4:
-                self.__possible.append((self.__x-2, self.__y+1))
+            if plateau[self._x-2][self._y+1] == 0:
+                self._possible.append((self._x-2, self._y+1))
 
-        if self.__x+2 < len(plateau) and self.__y-1 >= 0:
+        if self._x+2 < len(plateau) and self._y-1 >= 0:
             # Vérifie si la case est disponible
-            if plateau[self.__x+2][self.__y-1] == 0 or plateau[self.__x-2][self.__y-1] == 3 or plateau[self.__x-2][self.__y-1] == 4:
-                self.__possible.append((self.__x+2, self.__y-1))
+            if plateau[self._x+2][self._y-1] == 0:
+                self._possible.append((self._x+2, self._y-1))
 
-        if self.__x+2 < len(plateau) and self.__y+1 < len(plateau):
+        if self._x+2 < len(plateau) and self._y+1 < len(plateau):
             # Vérifie si la case est disponible
-            if plateau[self.__x+2][self.__y-1] == 0 or plateau[self.__x-2][self.__y-1] == 3 or plateau[self.__x-2][self.__y-1] == 4:
-                self.__possible.append((self.__x+2, self.__y+1))
+            if plateau[self._x+2][self._y-1] == 0:
+                self._possible.append((self._x+2, self._y+1))
 
-        if self.__x-1 >= 0 and self.__y-2 >= 0:
+        if self._x-1 >= 0 and self._y-2 >= 0:
             # Vérifie si la case est disponible
-            if plateau[self.__x-1][self.__y-2] == 0 or plateau[self.__x-2][self.__y-1] == 3 or plateau[self.__x-2][self.__y-1] == 4:
-                self.__possible.append((self.__x - 1, self.__y - 2))
+            if plateau[self._x-1][self._y-2] == 0:
+                self._possible.append((self._x - 1, self._y - 2))
 
-        if self.__x+1 < len(plateau) and self.__y-2 >= 0:
+        if self._x+1 < len(plateau) and self._y-2 >= 0:
             # Vérifie si la case est disponible
-            if plateau[self.__x+1][self.__y-2] == 0 or plateau[self.__x-2][self.__y-1] == 3 or plateau[self.__x-2][self.__y-1] == 4:
-                self.__possible.append((self.__x + 1, self.__y - 2))
+            if plateau[self._x+1][self._y-2] == 0:
+                self._possible.append((self._x + 1, self._y - 2))
 
-        if self.__x-1 >= 0 and self.__y+2 < len(plateau):
+        if self._x-1 >= 0 and self._y+2 < len(plateau):
             # Vérifie si la case est disponible
-            if plateau[self.__x-1][self.__y+2] == 0 or plateau[self.__x-2][self.__y-1] == 3 or plateau[self.__x-2][self.__y-1] == 4:
-                self.__possible.append((self.__x - 1, self.__y + 2))
+            if plateau[self._x-1][self._y+2] == 0:
+                self._possible.append((self._x - 1, self._y + 2))
 
-        if self.__x+1 < len(plateau) and self.__y+2 < len(plateau):
+        if self._x+1 < len(plateau) and self._y+2 < len(plateau):
             # Vérifie si la case est disponible
-            if plateau[self.__x+1][self.__y+2] == 0 or plateau[self.__x-2][self.__y-1] == 3 or plateau[self.__x-2][self.__y-1] == 4:
-                self.__possible.append((self.__x + 1, self.__y + 2))
+            if plateau[self._x+1][self._y+2] == 0 or plateau[self._x-2][self._y-1] == 3 or plateau[self._x-2][self._y-1] == 4:
+                self._possible.append((self._x + 1, self._y + 2))
 
     def deplacement_possible(self, plateau, x, y):
         """
         Fonction qui, si les coordonnées entrées sont égales aux coordonnées possibles, retourne True si elles sont égales et False sinon.
         """
         self.cases_possibles(plateau)
-        return (x, y) in self.__possible
+        return (x, y) in self._possible
 
 
 # ///////////////////////////////////////////////
 
 class Jeu(Pion):
     def __init__(self, board_size=8, nb_pions=4, x=1, y=1, joueur=1):
-        self.__board = []
-        self.__nb_pions = nb_pions
-        self.__board_size = board_size
+        self._board = []
+        self._nb_pions = nb_pions
+        self._board_size = board_size
         Pion.__init__(self, x, y, joueur)
 
     def get_board_size(self):
         """
         Permet d'avoir la taille du plateau.
         """
-        return self.__board_size
-
+        return self._board_size
 
     def set_board_size(self):
         """
         Permet de selectionner la taille du plateau entre 8 et 12.
         """
-        self.__board_size = int(input("Choisissez une taille de plateau comprise entre 8 et 12 : "))
-        while (8 > self.__board_size) or (self.__board_size > 12):
-            self.__board_size = int(
+        self._board_size = int(input("Choisissez une taille de plateau comprise entre 8 et 12 : "))
+        while (8 > self._board_size) or (self._board_size > 12):
+            self._board_size = int(
                 input("Taille non prise en charge, veuillez re-saisir une taille de plateau comprise entre 8 et 12 : "))
-        return self.__board_size
 
     def get_board(self):
         """
         Permet de retourner le tableau.
         """
-        return self.__board
+        return self._board
 
     def set_board(self):
         """
         Permet d'initialiser le plateau en fonction de la taille entrée.
         """
-        self.__board = []
+        self._board = []
         for i in range(self.get_board_size()):
             ligne = []
-            self.__board.append(ligne)
+            self._board.append(ligne)
             for j in range(self.get_board_size()):
                 ligne.append(0)
-        return self.__board
+        return self._board
 
     def get_nombre_de_pions_a_aligner(self):
         """
         Permet de retourner le nombre de pions nécessaire à aligner pour gagner.
         """
-        return self.__nb_pions
+        return self._nb_pions
 
     def set_nombre_de_pions_a_aligner(self):
         """
         Permet de définir le nombre de pions à aligner pour gagner.
         """
-        self.__nb_pions = int(input("Nombre de pion à aligner entre 4 et 6 : "))
+        self._nb_pions = int(input("Nombre de pion à aligner entre 4 et 6 : "))
 
     def placement_pion(self, plateau, x, y):
         """
@@ -138,7 +136,7 @@ class Jeu(Pion):
         Permet de vérifier les conditions d'arrêt
         """
         # Si la case choisie n'est pas une case où l'on peut mettre le pion (plus le cases disponibles) alors, la partie se termine.
-        # if not self.deplacement_possible(self.__board, x, y):
+        # if not self.deplacement_possible(self._board, x, y):
         # return True
         # elif :
 
@@ -147,40 +145,39 @@ class Jeu(Pion):
 
 class Gui(Jeu, Pion):
     def __init__(self, width=500, height=500, nb_pions=4):
-        self.__root = Tk()
-        self.__root.title("La puissance du cavalier")
+        self._root = Tk()
+        self._root.title("La puissance du cavalier")
         Jeu.__init__(self)
         self.nb_pion = nb_pions
-        self.__color_dict = {0: "white", 1: "bleu", 2: "red"}
-        self.__taille_case = 50
-        self.__plateau = self.set_board()
+        self._color_dict = {0: "white", 1: "bleu", 2: "red"}
+        self._taille_case = 50
+        self._plateau = self.get_board()
 
         # Définit la taille de la fenêtre
-        self.__canvas = Canvas(self.__root, width=width, height=height)
+        self._canvas = Canvas(self._root, width=width, height=height)
 
         # Affiche le plateau
         plateau_size = self.get_board_size()
-        print(plateau_size)
         for i in range(plateau_size):
             for j in range(plateau_size):
-                self.__canvas.create_rectangle(i * self.__taille_case + 50, j * self.__taille_case + 50,
-                                               i * self.__taille_case + self.__taille_case + 50,
-                                               j * self.__taille_case + self.__taille_case + 50)
+                self._canvas.create_rectangle(i * self._taille_case + 50, j * self._taille_case + 50,
+                                               i * self._taille_case + self._taille_case + 50,
+                                               j * self._taille_case + self._taille_case + 50)
 
-        self.__canvas.bind("<Button-1>", self.set_pions)
+        self._canvas.bind("<Button-1>", self.set_pions)
 
         # Lance le GUI
-        self.__canvas.pack()
-        self.__root.mainloop()
+        self._canvas.pack()
+        self._root.mainloop()
 
     def set_pions(self, event):
         """
         Fonction qui place les pions aux coordonnées du click sur le plateau.
         """
-        print(self.__plateau)
-        coord_x = (event.x-50) // self.__taille_case
-        coord_y = (event.y-50) // self.__taille_case
-        self.placement_pion(self.__plateau, coord_x, coord_y)
+        print(self._plateau)
+        coord_x = (event.x-50) // self._taille_case
+        coord_y = (event.y-50) // self._taille_case
+        self.placement_pion(self._plateau, coord_x, coord_y)
 
     def boucle_jeu(self):
         """
@@ -188,11 +185,5 @@ class Gui(Jeu, Pion):
         """
         pass
 
-
-game = Jeu(8)
-game.set_board_size()
-game.set_board()
-b = game.get_board()
-print(b, sep="\n")
 
 Gui(500, 500, 12)
